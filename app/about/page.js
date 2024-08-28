491e134900d1fe3028dbf2bef0d6cd9b260245b8
Noel Paing Oak Soe <u6530183@au.edu>
2024-08-28 13:33:38 +0700

    implemented frontend and backend

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const page = () => {
+  return <h1>Noel Paing Oak Soe</h1>
+}
+
+export default page
---
+++ app/vat/page.js
@@ -0,0 +1,63 @@
+'use client'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function Home() {
+  const { register, handleSubmit } = useForm()
+  const [vat, setVat] = React.useState(0)
+  const handleForm = (data) => {
+    console.debug(data)
+    const v = Math.round(data.price * data.rate * 100) / 100
+    setVat(v)
+  }
+  return (
+    <div>
+      <h1>VAT Calcuator</h1>
+      <form onSubmit={handleSubmit(handleForm)}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label>Price:</label>
+              </td>
+              <td>
+                <input
+                  type='number'
+                  name='price'
+                  {...register('price', { valueAsNumber: true })}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>VAT Rate:</label>
+              </td>
+              <td>
+                <input
+                  type='number'
+                  name='rate'
+                  value='0.07'
+                  readOnly
+                  {...register('rate', { valueAsNumber: true })}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan='2'>
+                <input type='submit' value='Calculate' />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>VAT:</label>
+              </td>
+              <td>
+                <input type='text' name='vat' value={process.env.VAT_RATE} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  )
+}
